dff24de130dca4e841f2ac36ce9150f4196b5e48
blackcat-007 <shubhodeepmukherjee24@gmail.com>
2026-08-28 10:18:12 +0530

    valeri against everyone solved in cf1000

---
+++ codeforces/codeforces practice/codeforces-1000/valeri_against_everyone.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<long long> a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        sort(a.begin(),a.end());
+        bool duplicate=false;
+        for(int i=1;i<n;i++){
+            if(a[i]==a[i-1]){
+                duplicate=true;
+                break;
+            }
+        }
+        if(duplicate){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+
+    }
+}
