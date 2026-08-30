72ed4aea2d668b42a3f4580e8173615ea50f0c99
blackcat-007 <shubhodeepmukherjee24@gmail.com>
2026-08-30 15:23:39 +0530

    fair numbers solved in cf1000

---
+++ codeforces/codeforces practice/codeforces-1000/fair_numbers.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isFair(long long x) {
+    long long temp = x;
+
+    while (temp > 0) {
+        int digit = temp % 10;
+        temp /= 10;
+
+        if (digit != 0 && x % digit != 0)
+            return false;
+    }
+
+    return true;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        long long n;
+        cin >> n;
+
+        while (!isFair(n)) {
+            n++;
+        }
+
+        cout << n << '\n';
+    }
+
+    return 0;
+}
